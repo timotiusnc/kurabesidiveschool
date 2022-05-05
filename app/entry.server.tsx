a7a9ffd05c5dd65b242f2b87481ed726bf9020d7
timotiusnc <timotius.nc@gmail.com>
2022-05-06 00:48:04 +0700

    Add proper security headers

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -14,6 +14,15 @@ export default function handleRequest(
 
   responseHeaders.set("Content-Type", "text/html");
 
+  responseHeaders.set("Content-Security-Policy", "upgrade-insecure-requests");
+  responseHeaders.set("X-Frame-Options", "deny");
+  responseHeaders.set("X-Content-Type-Options", "nosniff");
+  responseHeaders.set("Referer-Policy", "same-origin");
+  responseHeaders.set(
+    "Permissions-Policy",
+    "accelerometer=(), camera=(), gyroscope=(), magnetometer=(), microphone=(), payment=(), usb=(), interest-cohort=()"
+  );
+
   return new Response("<!DOCTYPE html>" + markup, {
     status: responseStatusCode,
     headers: responseHeaders,
