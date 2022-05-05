e09320b4b83b5f99fd2d6ba54e58648f659f4909
timotiusnc <timotius.nc@gmail.com>
2022-05-06 01:26:08 +0700

    Add fathom-client

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,4 +1,10 @@
 import { RemixBrowser } from "@remix-run/react";
+import { load } from "fathom-client";
 import { hydrate } from "react-dom";
 
 hydrate(<RemixBrowser />, document);
+
+load("JCUCMLDL", {
+  spa: "history",
+  excludedDomains: ["localhost"],
+});
